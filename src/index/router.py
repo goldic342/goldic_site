04a41aacced21719d14109466153a0ecbc01670e
goldic <contact@goldic.xyz>
2026-01-10 16:08:59 +0400

    Add favicon.ico route

--- src/index/router.py
+++ src/index/router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Request
+from fastapi.responses import FileResponse
 from fastapi.templating import Jinja2Templates
 
 
@@ -10,3 +11,8 @@ templates = Jinja2Templates(directory="templates")
 @router.get("/")
 async def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@router.get("/favicon.ico")
+async def favicon():
+    return FileResponse("static/images/favicon.ico")
