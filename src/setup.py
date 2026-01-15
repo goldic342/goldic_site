44e701871bf0b2a4d3faa2c7d64323adee2e96e0
goldic <contact@goldic.xyz>
2026-01-15 17:33:31 +0400

    Fix the setup script

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,13 @@
-from admin.service import AdminService
 from getpass import getpass
 from random import randbytes
+import hashlib
+
+
+def hash_password(password: str, salt: bytes, iter: int = 100_000) -> str:
+    hash = hashlib.pbkdf2_hmac("sha256", password.encode(), salt, iter)
+
+    return hash.hex()
+
 
 if __name__ == "__main__":
     password = getpass("Password: ")
@@ -9,7 +16,7 @@ if __name__ == "__main__":
     if not salt:
         salt = randbytes(24).hex()
 
-    hash = AdminService().hash_password(password, salt.encode())
+    hash = hash_password(password, salt.encode())
 
     print(f"SHA256 HASH: {hash}")
     print(f"Salt: {salt}")
